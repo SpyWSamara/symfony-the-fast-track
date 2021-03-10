b1f0aae52c86a10cb20a595c1699b2b5932c25ab
SpyWSamara <spywsamara@gmail.com>
2021-03-11 01:24:19 +0400

    Test: add comment for conference

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -29,4 +29,17 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertSelectorTextContains('h2', 'Samara 2020');
         $this->assertSelectorExists('div:contains("There are 1 comments")');
     }
+
+    public function testCommentSubmission(): void {
+        $client = static::createClient();
+        $client->request('GET', '/conference/samara-2020');
+        $client->submitForm('Submit', [
+            'comment_form[author]' => 'PhpUnit',
+            'comment_form[text]' => 'Functional testing in action!',
+            'comment_form[email]' => 'testing@local.tld',
+        ]);
+        $this->assertResponseRedirects('/conference/samara-2020');
+        $client->followRedirect();
+        $this->assertSelectorExists('div:contains("There are 2 comments")');
+    }
 }
