0dfda5379638301da3abed49e6eec527c296b4a6
SpyWSamara <spywsamara@gmail.com>
2021-03-11 01:16:00 +0400

    Test: open detail conference test from home page

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -14,4 +14,19 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Give your feedback');
     }
+
+    public function testConferencePage(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertCount(2, $crawler->filter('h4'));
+
+        $client->clickLink('View');
+
+        $this->assertPageTitleContains('Samara');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Samara 2020');
+        $this->assertSelectorExists('div:contains("There are 1 comments")');
+    }
 }
