3fdf53845cc2c201906e653cdd74b18a39d7f566
SpyWSamara <spywsamara@gmail.com>
2021-03-23 00:33:01 +0400

    Rewrite ConferenceControllerTest with comment statuses
    Now two steps for send comment:
    - add comment to conference;
    - publish comment.

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -2,6 +2,8 @@
 
 namespace App\Tests\Controller;
 
+use App\Repository\CommentRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ConferenceControllerTest extends WebTestCase
@@ -33,12 +35,19 @@ class ConferenceControllerTest extends WebTestCase
     public function testCommentSubmission(): void {
         $client = static::createClient();
         $client->request('GET', '/conference/samara-2020');
+        $email = 'testing@local.tld';
         $client->submitForm('Submit', [
             'comment_form[author]' => 'PhpUnit',
             'comment_form[text]' => 'Functional testing in action!',
-            'comment_form[email]' => 'testing@local.tld',
+            'comment_form[email]' => $email,
         ]);
         $this->assertResponseRedirects('/conference/samara-2020');
+
+        // simulate comment validation
+        $comment =self::$container->get(CommentRepository::class)->findOneByEmail($email);
+        $comment->setState('published');
+        self::$container->get(EntityManagerInterface::class)->flush();
+
         $client->followRedirect();
         $this->assertSelectorExists('div:contains("There are 2 comments")');
     }
