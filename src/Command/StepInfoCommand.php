f17ffbf6ab0cfb4113af16392fab0fc2f732e2e2
SpyWSamara <spywsamara@gmail.com>
2021-04-07 02:13:08 +0400

    Cache command `app:step:info` for 30 second

--- src/Command/StepInfoCommand.php
+++ src/Command/StepInfoCommand.php
@@ -2,15 +2,26 @@
 
 namespace App\Command;
 
+use Psr\Cache\CacheItemInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
+use Symfony\Contracts\Cache\CacheInterface;
 
 class StepInfoCommand extends Command
 {
     protected static $defaultName = 'app:step:info';
     protected static string $defaultDescription = 'Show current tags list';
+
+    private CacheInterface $cache;
+
+    public function __construct(CacheInterface $cache)
+    {
+        $this->cache = $cache;
+
+        parent::__construct();
+    }
 
     protected function configure()
     {
@@ -19,9 +30,18 @@ class StepInfoCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $process = new Process(['git', 'tag', '-l', '--points-at', 'HEAD']);
-        $process->mustRun();
-        $output->write($process->getOutput());
+        $step = $this->cache->get(
+            'app.current_step',
+            function (CacheItemInterface $item) {
+                $process = new Process(['git', 'tag', '-l', '--points-at', 'HEAD']);
+                $process->mustRun();
+                $item->expiresAfter(30);
+
+                return $process->getOutput();
+            }
+        );
+
+        $output->write($step);
 
         return Command::SUCCESS;
     }
