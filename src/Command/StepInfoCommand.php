0ce5872d9326db1bb58a785d0222520f7e3c8d86
SpyWSamara <spywsamara@gmail.com>
2021-04-07 02:04:07 +0400

    Add command for show current git tags

---
+++ src/Command/StepInfoCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+
+class StepInfoCommand extends Command
+{
+    protected static $defaultName = 'app:step:info';
+    protected static string $defaultDescription = 'Show current tags list';
+
+    protected function configure()
+    {
+        $this->setDescription(self::$defaultDescription);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $process = new Process(['git', 'tag', '-l', '--points-at', 'HEAD']);
+        $process->mustRun();
+        $output->write($process->getOutput());
+
+        return Command::SUCCESS;
+    }
+}
