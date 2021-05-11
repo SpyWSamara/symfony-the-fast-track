b4a56c70133a13c25735629b634382a2e356b2a8
SpyWSamara <spywsamara@gmail.com>
2021-05-12 02:30:46 +0400

    Update SPA main code for use API as conference source

--- spa/src/app.js
+++ spa/src/app.js
@@ -1,12 +1,24 @@
 import {h, render} from 'preact';
 import {Router, Link} from 'preact-router';
+import {useState, useEffect} from "preact/hooks";
 
 import '../assets/styles/app.scss';
 
+import {findConferences} from './api/api';
 import Home from './pages/home';
 import Conference from './pages/conference';
 
 function App() {
+    const [conferences, setConferences] = useState(null);
+
+    useEffect(_ => {
+        findConferences().then(setConferences);
+    }, []);
+
+    if (null === conferences) {
+        return <div className="text-center pt-5">Loading...</div>
+    }
+
     return (
         <div>
             <header className="header">
@@ -19,16 +31,18 @@ function App() {
                 </nav>
                 <nav className="bg-light bg-bottom text-center">
                     <div className="container">
-                        <Link className="nav-conference" href="/conference/samara2021">
-                            Samara 2021
-                        </Link>
+                        {conferences.map(conference => (
+                            <Link className="nav-conference" href={'/conference/' + conference.slug}>
+                                {conference.city} {conference.year}
+                            </Link>
+                        ))}
                     </div>
                 </nav>
             </header>
 
             <Router>
-                <Home path="/"/>
-                <Conference path="/conference/samara2021"/>
+                <Home path="/" conferences={conferences}/>
+                <Conference path="/conference/:slug" conferences={conferences}/>
             </Router>
         </div>
     );
